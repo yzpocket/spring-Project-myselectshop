44dcfd320a584c18b3f9ffa2e70d3fc35aad7058
INYONG KIM <ohnyong87@gmail.com>
2023-09-12 23:21:59 +0900

    test : Use Mockito Test on ProductService update edges

---
+++ src/test/java/com/sparta/myselectshop/service/ProductServiceTest.java
@@ -0,0 +1,89 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.FolderRepository;
+import com.sparta.myselectshop.repository.ProductFolderRepository;
+import com.sparta.myselectshop.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class) // @Mock 사용을 위해 설정합니다. <- 가짜객체(주입용) 생성용
+class ProductServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    FolderRepository folderRepository;
+
+    @Mock
+    ProductFolderRepository productFolderRepository;
+
+    @Test
+    @DisplayName("관심 상품 희망가 - 최저가 이상으로 변경")
+    void test1() {
+        // given
+        Long productId = 100L;
+        int myprice = ProductService.MIN_MY_PRICE + 3_000_000;
+
+        ProductMypriceRequestDto requestMyPriceDto = new ProductMypriceRequestDto();
+        requestMyPriceDto.setMyprice(myprice);
+
+        User user = new User(); //
+        ProductRequestDto requestProductDto = new ProductRequestDto(
+                "Apple <b>맥북</b> <b>프로</b> 16형 2021년 <b>M1</b> Max 10코어 실버 (MK1H3KH/A) ",
+                "https://shopping-phinf.pstatic.net/main_2941337/29413376619.20220705152340.jpg",
+                "https://search.shopping.naver.com/gate.nhn?id=29413376619",
+                3515000
+        );
+
+        Product product = new Product(requestProductDto, user);
+
+        ProductService productService = new ProductService(productRepository, folderRepository, productFolderRepository);
+
+        given(productRepository.findById(productId)).willReturn(Optional.of(product)); // 원본 코드에서는 이 부분이 NullpointException이 발생하는데
+        // 가짜 객체를 만들고, 예외가 발생하지 않도록 구체적으로 작성해줌. 테스트 목적이 예외를 체크하기 위함이 아니기 때문에. -> 엣지포인트마다 다르다고 표현함
+
+        // when
+        ProductResponseDto result = productService.updateProduct(productId, requestMyPriceDto);
+
+        // then
+        assertEquals(myprice, result.getMyprice());
+    }
+
+    @Test
+    @DisplayName("관심 상품 희망가 - 최저가 미만으로 변경")
+    void test2() {
+        // given
+        Long productId = 200L;
+        int myprice = ProductService.MIN_MY_PRICE - 50;
+
+        ProductMypriceRequestDto requestMyPriceDto = new ProductMypriceRequestDto();
+        requestMyPriceDto.setMyprice(myprice);
+
+        ProductService productService = new ProductService(productRepository, folderRepository, productFolderRepository);
+
+        // when
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            productService.updateProduct(productId, requestMyPriceDto);
+        });
+
+        // then
+        assertEquals(
+                "유효하지 않은 관심 가격입니다. 최소 " +ProductService.MIN_MY_PRICE + " 원 이상으로 설정해 주세요.",
+                exception.getMessage()
+        );
+    }
+}
